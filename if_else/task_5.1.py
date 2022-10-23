2658bf2f83091632c79ae8308da0b3c8480b04ea
zharabtsou <zharabtsou@tut.by>
2022-10-23 15:42:29 +0300

    feat: выучил if el/elif/else

---
+++ if_else/task_5.1.py
@@ -0,0 +1,12 @@
+car = 'subaru'
+if car == 'subaru':
+    print(f'Is car {car.title()}. I predict True.')
+
+if car != 'audi':
+    print(f'\nIs car {car.title()}. I predict False.')
+
+
+
+
+print(car == 'subaru')
+print(car == 'audi')
