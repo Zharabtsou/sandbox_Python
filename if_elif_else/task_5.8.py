7a3ffc8af8af8ddcfec3d41fae25ccec3ca53b00
zharabtsou <32492991+Zharabtsou@users.noreply.github.com>
2022-10-27 10:50:02 +0300

    feat: в начале программы проверяется наичие пользователей в списке если он пуст предлагает регестрацию

--- if_elif_else/task_5.8.py
+++ if_elif_else/task_5.8.py
@@ -2,9 +2,23 @@ list_user = []
 list_password = []
 new_user = True
 while new_user:
-    print('1 - добавить нового пользователя')
-    print('2 - войти')
-    print('3 - окончание сеанса')
-    var = input('Введите: ')
     if not list_user:
-        print('список пуст')
+        print('Список пользователь пуст создайте нового пользователя')
+        user = input('Введите имя нового пользователя: ')
+        list_user.append(user)
+        user_password = input('Введите пароля: ')
+        list_password.append(user_password)
+    print('добрый день вас приветсвуент программа эмулятор регистрации пользователей')
+    print('1 - добавить нового пользователя')
+    print('2 - удалить пользователя')
+    print('3 - войти')
+    print('4 - окончание сеанса')
+    var = int(input('Введите: '))
+
+
+
+    if var == 4:
+        print('Спасибо за работу досвидание')
+        break
+
+
