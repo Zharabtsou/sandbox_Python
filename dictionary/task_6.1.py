f0f5d250333cbeb09c9587534abfd45e8d65149e
zharabtsou <zharabtsou@tut.by>
2022-10-29 13:52:41 +0300

    feat: начал изучать словари

---
+++ dictionary/task_6.1.py
@@ -0,0 +1,23 @@
+human = {
+    'first_name' : 'Андрей',
+    'last_name': 'Жеребцов',
+    'age': '32',
+    'citi': 'Minsk',
+    }
+
+#вывод значение словоря по ключу
+print(human['first_name'])
+print(human['last_name'])
+print(human['age'])
+print(human['citi'])
+print()
+#вывод значений словора через запрос get + ключь
+print(human.get('first_name'))
+print(human.get('last_name'))
+print(human.get('age'))
+print(human.get('citi'))
+print()
+
+# get полезен тем что при отсутсвии такого ключа или значения в ключе можно прописать что вывести или вывести значение по умолчанию "None"
+print(human.get('ceti'))
+print(human.get('ceti', 'ceti!!!'))
