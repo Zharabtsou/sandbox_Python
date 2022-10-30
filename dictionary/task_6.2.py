6d9bf410bcb842f08365c56e9765d229b284adea
zharabtsou <zharabtsou@tut.by>
2022-10-30 13:45:58 +0300

    feat: изучаю работу со словорями

---
+++ dictionary/task_6.2.py
@@ -0,0 +1,11 @@
+user_name = {
+    'Dima': '5',
+    'Kira': 1,
+    'Sveta': 2,
+    'vasia': 'lake',
+
+    }
+
+print(user_name.get('Dima'))
+print(user_name.get('Kira'))
+print(user_name.get('vasia'))
