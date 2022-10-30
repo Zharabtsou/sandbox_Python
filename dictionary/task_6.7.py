cf89096faf3a560b1ad4762128fd445761c10d21
zharabtsou <zharabtsou@tut.by>
2022-10-30 21:41:35 +0300

    feat: учу множества и выпод информации из обьедененнных словарей в списки с писки со вложенными словорями

---
+++ dictionary/task_6.7.py
@@ -0,0 +1,23 @@
+human_01 = {
+    'first_name' : 'Андрей',
+    'last_name': 'Жеребцов',
+    'age': '32',
+    'citi': 'Minsk',
+    }
+human_02 = {
+    'first_name' : 'иванов',
+    'last_name': 'Иван',
+    'age': '22',
+    'citi': 'Gomel',
+    }
+human_03 = {
+    'first_name' : 'Александр',
+    'last_name': 'Кряжев',
+    'age': '31',
+    'citi': 'Jlobin',
+    }
+people = [human_01, human_02, human_03]
+
+for item in people:
+    print(item.values())
+
