86d3be3ca6ffce9fed4ac382ff7a6213da64efda
zharabtsou <zharabtsou@tut.by>
2022-11-03 21:28:40 +0300

    feat: учим функции

---
+++ 8_functions/task_8.1.py
@@ -0,0 +1,12 @@
+def make_shit(size, text):
+    """
+    Это коментарий к функции
+    :param size:
+    :param text:
+    :return:
+    """
+    print(f'\nразмер футболки "{size}"')
+    print(f'надпись на футболке "{text}"')
+
+make_shit(182,'AC/DC')
+make_shit(text="не важно какой текст", size= 152)
