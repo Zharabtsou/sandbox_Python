9508b5e44267eadd7ca0c5d8f9919ce94fca4567
zharabtsou <32492991+Zharabtsou@users.noreply.github.com>
2022-11-15 12:49:34 +0300

    feat: не как не разоберусь с те как в пользоватьльскую функцию добавить параметарь по типу словаря

---
+++ 8_import/task_8.9.py
@@ -0,0 +1,13 @@
+def person(first_name, last_name):
+    persone = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return persone
+
+
+min = person('gimi','gens', age = 27)
+
+
+print(min)
+
+#так и не понял что тут не так(
---
+++ 9_class/task_9.1.py
@@ -0,0 +1,11 @@
+class Restaurant():
+
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def descride_restaurant(self):
+        pass
+
+    def open_restaurant(self):
+        pass
