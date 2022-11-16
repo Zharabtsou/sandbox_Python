405f6813b401de5644dd2b46e8d47e1f27eca25d
zharabtsou <zharabtsou@tut.by>
2022-11-16 21:48:34 +0300

    feat: продолжаем изучать работу с классами расширение и подключение доп класов!!! не наследование а подключение доп классов

--- 9_class/task_9.3-1.py
+++ 9_class/task_9.3-1.py
@@ -24,15 +24,18 @@ class Car():
 class ElectricCar(Car):
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
-
-    def battery_size(self, size):
-        self.size = size
+        self.battary = Battary()
 
     def geet_battery(self):
-        print(f'{self.make} установлена батарея размером {self.size} литров')
+        print(f'{self.make} установлена батарея размером {self.battary.size} литров')
 
     def get_fill(self):
-        print(f'У электромобиля марки {self.make} нет бака!!! Там батарея размером {self.size} Ah')
+        print(f'У электромобиля марки {self.make} нет бака!!! Там батарея размером {self.battary.size} Ah')
+
+class Battary():
+
+    def battery_size(self, size=100):
+        self.size = size
 
 
 car1 = Car('Audi', 'C8', 2015)
@@ -43,7 +46,7 @@ car1.read_odometer_reading()
 car2 = ElectricCar('tesla', 'T2', 2020)
 print(car2.get_descriptive_name())
 car2.read_odometer_reading()
-car2.battery_size(5500)
+car2.battary.battery_size(5500)
 car2.geet_battery()
 car2.increment_odometer(1000)
 car2.read_odometer_reading()
