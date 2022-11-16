905f44c1bcf3657a84f9c2b6a24ecf454ecbceaa
zharabtsou <zharabtsou@tut.by>
2022-11-16 22:57:13 +0300

    feat: продолжаем разбиратся в классах и их импорте

--- 9_class/task_9.4.py
+++ 9_class/task_9.4.py
@@ -3,8 +3,7 @@ from electric_car import ElectricCar
 
 car3 = ElectricCar('tesla', 'T2', 2020)
 print(car3.get_descriptive_name())
-car3.read_odometer_reading()
-car3.battary.battery_size(5500)
-car3.geet_battery()
-car3.increment_odometer(1000)
-car3.read_odometer_reading()
+print()
+
+my_car= ElectricCar('BMW', 'E34', 2022)
+print(my_car.get_descriptive_name())
