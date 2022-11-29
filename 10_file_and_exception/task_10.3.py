63cfb67eb59b0ad5b9f359b5a62ea87d1047fdae
zharabtsou <32492991+Zharabtsou@users.noreply.github.com>
2022-11-29 15:20:14 +0300

    feat: сумма чисел с избижанием ошибок

--- 10_file_and_exception/task_10.3.py
+++ 10_file_and_exception/task_10.3.py
@@ -3,7 +3,7 @@ def summa_int(num1, num2):
         num1 = int(num1)
         num2 = int(num2)
     except ValueError:
-        print(f'введите число')
+        print(f'введите число!!!!')
     else:
         print(num1+num2)
 
