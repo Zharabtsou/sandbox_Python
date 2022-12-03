e7b29d6d365d0360b238da65b863132d46c2968b
zharabtsou <zharabtsou@tut.by>
2022-12-03 23:40:54 +0300

    feat: функция json

--- 10_file_and_exception/json/remember_me.py
+++ 10_file_and_exception/json/remember_me.py
@@ -1,13 +1,31 @@
 import json
 
-filename = 'username.json'
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
+def get_stored_username():
+    """Получает храниемое имя пользователя если оно существует"""
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    """Запрашивает новое имя пользователя если его нет в файле"""
     username = input('Как тебя зовут? ')
+    filename = 'username.json'
     with open(filename, 'w') as f:
         json.dump(username, f)
+    return username
+
+def greet_user():
+    '''Приветсвует пользователя по именни'''
+    username = get_stored_username()
+    if username:
+        print(f'С возвращением {username}!')
+    else:
+        username = get_new_username()
         print(f'Мы тебя запомним если ты вернешся, {username}!')
-else:
-    print(f'С возвращением {username}!')
+
+greet_user()
